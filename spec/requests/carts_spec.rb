c378ad6fb3eac66f23495449f770f305e59166df
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-01-29 12:17:29 -0300

    created specs for cart item, changed specs for using a reusable let variable

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "/carts", type: :request do
-  pending "TODO: Escreva os testes de comportamento do controller de carrinho necessários para cobrir a sua implmentação #{__FILE__}"
-  describe "POST /add_items" do
-    let(:cart) { Cart.create }
-    let(:product) { Product.create(name: "Test Product", price: 10.0) }
-    let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 1) }
+RSpec.describe "/cart", type: :request do
+  describe "POST /cart/add_item" do
+    let(:cart) { Cart.create!(total_price: 0) }
+    let(:product) { Product.create!(name: "Test Product", price: 10.0) }
+    let!(:cart_item) { cart.cart_items.create!(product: product, quantity: 1) }
 
-    context 'when the product already is in the cart' do
-      subject do
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
+    context 'when the product is already in the cart' do
+      before do
+        post "/cart/add_item", params: { product_id: product.id, quantity: 1 }, as: :json
       end
 
       it 'updates the quantity of the existing item in the cart' do
-        expect { subject }.to change { cart_item.reload.quantity }.by(2)
+        expect(cart_item.reload.quantity).to eq(2)
       end
     end
   end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe "/products", type: :request do
 
   let(:invalid_attributes) {
     {
-      price: -1
+      name: "Invalid Product",
+      price: -1 
     }
   }
 
@@ -39,13 +40,13 @@ RSpec.describe "/products", type: :request do
       it "creates a new Product" do
         expect {
           post products_url,
-               params: { product: valid_attributes }, headers: valid_headers, as: :json
+                params: { product: valid_attributes }, headers: valid_headers, as: :json
         }.to change(Product, :count).by(1)
       end
 
       it "renders a JSON response with the new product" do
         post products_url,
-             params: { product: valid_attributes }, headers: valid_headers, as: :json
+              params: { product: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
@@ -55,13 +56,13 @@ RSpec.describe "/products", type: :request do
       it "does not create a new Product" do
         expect {
           post products_url,
-               params: { product: invalid_attributes }, as: :json
+                params: { product: invalid_attributes }, as: :json
         }.to change(Product, :count).by(0)
       end
 
       it "renders a JSON response with errors for the new product" do
         post products_url,
-             params: { product: invalid_attributes }, headers: valid_headers, as: :json
+              params: { product: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
